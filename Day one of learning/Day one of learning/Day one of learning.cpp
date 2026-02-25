41219e35ae87cc4ccde742de32e12958c4f5a854
boss-kali <gamingroodetc@gmail.com>
2026-02-25 12:15:39 +0300

    test

---
+++ Day one of learning/Day one of learning/Day one of learning.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int num;
+cout << "Enter a number: ";
+cin >> num;
+if (num % 2 == 0) {
+	cout << " the number is even.  " << endl;
+}
+else {
+	cout << " the number is odd. ' " << endl;
+}
+}
---
+++ How to take input/How to take input/How to take input.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << " enter first number : ";
+    cin >> num1;
+	cout << " enter second number : ";
+    cin >> num2;
+    cout << " sum of two numbers is : " << num1 + num2;
+	return 0;
+    // اتجاه الاسهم هو المهم
+
+}
+
+
