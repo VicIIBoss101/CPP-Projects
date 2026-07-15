7077976423afa028e970da0a646fa1ae7cc233c2
VicIIBoss101 <abbasroyale2@gmail.com>
2026-07-15 14:20:18 +0300

    how to make sum function

--- Learn cpp/basics/Function-1/Function-1.cpp
+++ Learn cpp/basics/Function-1/Function-1.cpp
@@ -1,20 +1,17 @@
-// Function-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include <string>
+#include <print>
+using namespace std;
+int sum(int num1, int num2) {
+    return (num1 + num2);
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    int num1, num2, sumN;
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "\n" << "Enter the second number: ";
+    cin >> num2;
+    sumN = sum(num1, num2);
+    std::print("the sum is: {}" , sumN);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
